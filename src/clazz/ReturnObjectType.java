fdb776a71f0b9b62aa29658127cc95ceec099441
= <=>
2024-04-01 21:37:44 +0900

    4/1 추가

---
+++ src/clazz/ReturnObjectType.java
@@ -0,0 +1,21 @@
+package clazz;
+
+public class ReturnObjectType {
+   public Data getData(String id, String name){
+      Data d = new Data();
+      d.id = id;
+      d.name = name;
+      return d;
+   }
+   public static void main(String[] args) {
+      ReturnObjectType obj = new ReturnObjectType();
+      Data r = obj.getData("a001","감자");
+      System.out.println("ID : " + r.id);
+      System.out.println("Name : " + r.name);
+   }
+}
+
+class Data{
+   String id;
+   String name;
+}
