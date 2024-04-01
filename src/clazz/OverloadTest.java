241a254bc46f4eaf58f642ab10930332550daa18
= <=>
2024-04-01 23:58:22 +0900

    2024/04/01 마지막 작업

--- src/clazz/OverloadTest.java
+++ src/clazz/OverloadTest.java
@@ -1,5 +1,29 @@
 package clazz;
 
 public class OverloadTest {
-   
+   String id="hong";
+   String pwd="1111";
+
+   public void conn(){ 
+      print(); 
+   }
+   public void conn(String id){
+      this.id = id;
+      conn();
+   }
+   public void conn(String id, String pwd){
+      this.id = id;
+      this.pwd = pwd;
+      print();
+   }
+   public void print(){
+      System.out.printf("name=%s, pwd=%s 를 사용하여 연결함\n", id, pwd);
+   }
+
+   public static void main(String[] args) {
+      OverloadTest ot = new OverloadTest();
+      ot.conn();
+      ot.conn("kim");
+      ot.conn("park", "1234");
+   }
 }
