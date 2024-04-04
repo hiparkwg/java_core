a7745e1226208ed66aa4d4f72d4ea9362121f7c5
jobtc <hiparkwg@gmail.com>
2024-04-04 14:25:27 +0900

    후속 작업

---
+++ src/clazz/ParamMulti.java
@@ -0,0 +1,31 @@
+package clazz;
+
+public class ParamMulti {
+    public void paramSum(int... x){
+        int sum = 0;
+        for(int k : x){
+            sum += k;
+        }
+        System.out.println("sum=" + sum);
+    }
+
+    public void paramSum2(int[] x){
+        int sum = 0;
+        for(int k : x){
+            sum += k;
+        }
+        System.out.println("sum=" + sum);
+
+    }
+
+    public static void main(String[] args) {
+        ParamMulti pm = new ParamMulti();
+        pm.paramSum(10,20);
+        pm.paramSum(10,20,30);
+        pm.paramSum(); // 가능
+
+        pm.paramSum2(new int[]{10,20});
+        pm.paramSum2(new int[]{10,20,30});
+       //pm.paramSum2(); // 오류
+    }
+}
---
+++ src/clazz/ParamTest.java
@@ -0,0 +1,21 @@
+package clazz;
+
+public class ParamTest {
+    public void param(int x, int y){
+        int r = x + y;
+        System.out.println("x+y=" + r);
+    }
+
+    public double param(double x, double y){
+        double r = x+y;
+        return r;
+    }
+
+    public static void main(String[] args) {
+        ParamTest pt = new ParamTest();
+        pt.param(10,20);
+
+        double k = pt.param(10.0,20.0);
+        System.out.println("k=" + k);
+    }
+}
