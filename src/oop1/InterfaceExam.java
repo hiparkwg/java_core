8c075821a6c66c3c5652ab4034a153402b966969
= <=>
2024-04-07 23:41:57 +0900

    다시 작업

--- src/oop1/InterfaceExam.java
+++ src/oop1/InterfaceExam.java
@@ -7,15 +7,43 @@ public class InterfaceExam implements MyInter1{
 
    @Override
    public void sum(int x, int y) {
-
+      System.out.println(x+y);
    }
 
    @Override
    public void div(int x, int y) {
-
+      System.out.println(x/(double)y);
    }
-   
+
+
+   public static void main(String[] args) {
+      InterfaceExam ex = new InterfaceExam();
+      ex.sum(10,20);
+      ex.div(10,2);
+      //ex.name = "park";//상수이므로 값을 수정할 수 없음.
+   }
 }
+
+class ExtendsInterExam implements ExtendsInterface{
+
+   @Override
+   public void sum(int x, int y) {
+   }
+
+   @Override
+   public void div(int x, int y) {
+   }
+
+   @Override
+   public void mul(int x, int y) {
+   }
+
+   @Override
+   public void sub(int x, int y) {
+   }
+}
+
+interface ExtendsInterface extends MyInter1, MyInter2{ }
 
 interface MyInter1{
    String name = "my interface1";
