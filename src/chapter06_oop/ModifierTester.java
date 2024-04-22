c71b0b3792f50d1f6bb8dc026b659d2170be012f
= <=>
2024-04-22 23:09:01 +0900

    같은 패키지 내에 있는 클래스의 접근권한별 접근 테스트

--- src/chapter06_oop/ModifierTester.java
+++ src/chapter06_oop/ModifierTester.java
@@ -1,11 +1,20 @@
+/*
+ * 같은 패키지 내에 있는 클래스의 접근권한별 접근 테스트
+ */
+
 package chapter06_oop;
 
 public class ModifierTester {
-    public static void main(String[] args) {
+    public void test(){
         ModifierTest mt = new ModifierTest();
-        mt.anyBody();
-        mt.myFamily();
-        mt.inFolder();
-        //mt.inMy();  
+        mt.anyBody(); // public type call
+        mt.myFamily(); // protected type call
+        mt.inFolder(); // defaul type call
+        //mt.inMy();  // private type call
+
+
+    }
+    public static void main(String[] args) {
+        new ModifierTester().test();
     }
 }
